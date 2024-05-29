56cbd155f73aeaa9f963c96cef82511de8801ceb
sttiwahm <tina.wahmkow@student.hs-anhalt.de>
2024-05-29 15:19:05 +0200

    insert statements

---
+++ insert-statements.sql
@@ -0,0 +1,7 @@
+INSERT ALL
+   INTO flowers (id, name, latin_name, color, image, description) VALUES (1, 'Akazie', 'Acacieae', 'white', '1_Acacieae_white', 'Die Akazien (Acacieae) sind eine Tribus in der Familie der Hülsenfrüchtler.')
+   INTO flowers (id, name, latin_name, color, image, description) VALUES (2, 'Akazie', 'Acacieae', 'pink', '2_Acacieae_pink', 'Dieser Kleinbaum trumpft durch seine traumhaft schönen Blüten in zartem Rosa auf.')
+   INTO flowers (id, name, latin_name, color, image, description) VALUES (3, 'Alpenrose', 'Rhododendron ferrugineum', 'pink', '3_Rhododendron_ferrugineum', 'Die Rostblättrige Alpenrose (Rhododendron ferrugineum) ist eine Pflanzenart aus der Gattung der Rhododendren.')
+   INTO flowers (id, name, latin_name, color, image, description) VALUES (4, 'Windröschen', 'Anemone', 'white', '4_Anemone', 'Die Garten-Herbst-Anemone (Anemone japonica) blüht im Spätsommer und Herbst mit weißen Blüten.')
+   INTO flowers (id, name, latin_name, color, image, description) VALUES (5, 'Aster', 'Asteraceae', 'purple', '5_Asteraceae', 'Die Astern (Aster) sind mit über 180 bekannten Arten eine der artenreichsten Pflanzengattungen.')
+SELECT 1 FROM DUAL;
