b325eab82387d2eff7581c72e323dbec7526718a
sttiwahm <tina.wahmkow@student.hs-anhalt.de>
2024-05-29 15:23:22 +0200

    mehr inserts

--- insert-statements.sql
+++ insert-statements.sql
@@ -5,3 +5,18 @@ INSERT ALL
    INTO flowers (id, name, latin_name, color, image, description) VALUES (4, 'Windröschen', 'Anemone', 'white', '4_Anemone', 'Die Garten-Herbst-Anemone (Anemone japonica) blüht im Spätsommer und Herbst mit weißen Blüten.')
    INTO flowers (id, name, latin_name, color, image, description) VALUES (5, 'Aster', 'Asteraceae', 'purple', '5_Asteraceae', 'Die Astern (Aster) sind mit über 180 bekannten Arten eine der artenreichsten Pflanzengattungen.')
 SELECT 1 FROM DUAL;
+
+INSERT ALL
+    INTO categories (name) VALUES ('Liebe')
+    INTO categories (name) VALUES ('Schutz')
+    INTO categories (name) VALUES ('Unsterblichkeit')
+    INTO categories (name) VALUES ('Glückseligkeit')
+    INTO categories (name) VALUES ('Freundschaft')
+    INTO categories (name) VALUES ('Romantik')
+    INTO categories (name) VALUES ('Sehnsucht')
+    INTO categories (name) VALUES ('Vorfreude')
+    INTO categories (name) VALUES ('Freude')
+    INTO categories (name) VALUES ('Verlassenheit')
+    INTO categories (name) VALUES ('Unverbindlichkeit')
+    INTO categories (name) VALUES ('Gleichgültigkeit')
+SELECT 1 from dual
